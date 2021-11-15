46e16c1b6d06d3f733731cd9abe3aeaee2a35055
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-11-15 16:31:14 +0600

    fix lazy

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import ClipLoader from "react-spinners/HashLoader";
 import { css } from "@emotion/react";
 
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import ManhatonSalon from "./pages/ManhatonSalon";
-// import HamptonSalon from "./pages/HamptonSalon";
-// import NotFound from "./pages/404";
-
 const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 const ManhatonSalon = lazy(() => import("./pages/ManhatonSalon"));
 const HamptonSalon = lazy(() => import("./pages/HamptonSalon"));
-// const NotFound = lazy(() => import("./pages/404"));
+const NotFound = lazy(() => import("./pages/404"));
 
 const App = () => {
   const override = css`
@@ -23,20 +17,21 @@ const App = () => {
     border-color: red;
   `;
 
-  const renderLoader = () => (
-    <p>
-      <ClipLoader css={override} size={120} />
-    </p>
-  );
   return (
     <Switch>
-      <Suspense fallback={renderLoader()}>
+      <Suspense
+        fallback={
+          <h1>
+            <ClipLoader css={override} size={120} />
+          </h1>
+        }
+      >
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/contact/manhaton" component={ManhatonSalon} />
         <Route path="/contact/hampton" component={HamptonSalon} />
-        {/* <Route path="" component={NotFound} /> */}
       </Suspense>
+      <Route path="" component={NotFound} />
     </Switch>
   );
 };
