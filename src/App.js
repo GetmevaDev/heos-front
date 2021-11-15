fc941dc1774d9db030e3f3413feff1982e63fa78
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-11-15 16:15:11 +0600

    lazy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import ClipLoader from "react-spinners/HashLoader";
+import { css } from "@emotion/react";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import ManhatonSalon from "./pages/ManhatonSalon";
-import HamptonSalon from "./pages/HamptonSalon";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import ManhatonSalon from "./pages/ManhatonSalon";
+// import HamptonSalon from "./pages/HamptonSalon";
 import NotFound from "./pages/404";
 
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const ManhatonSalon = lazy(() => import("./pages/ManhatonSalon"));
+const HamptonSalon = lazy(() => import("./pages/HamptonSalon"));
+
 const App = () => {
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    top: 300px;
+    border-color: red;
+  `;
+
+  const renderLoader = () => (
+    <p>
+      <ClipLoader css={override} size={120} />
+    </p>
+  );
   return (
     <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
-      <Route path="/contact/manhaton" component={ManhatonSalon} />
-      <Route path="/contact/hampton" component={HamptonSalon} />
-      <Route component={NotFound} />
+      <Suspense fallback={renderLoader()}>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contact/manhaton" component={ManhatonSalon} />
+        <Route path="/contact/hampton" component={HamptonSalon} />
+        <Route component={NotFound} />
+      </Suspense>
     </Switch>
   );
 };
